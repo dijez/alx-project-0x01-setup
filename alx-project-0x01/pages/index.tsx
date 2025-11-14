5b7486af25b0c80d2dbfe79f45449dec9421525e
dijez <khadijaahmad2k21@gmail.com>
2025-11-14 07:20:12 +0100

    update

---
+++ alx-project-0x01/pages/index.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const Home: React.FC = () => {
+  return (
+  <div className="flex justify-center item-center h-screen">
+    <h1 className="text-7x1 font-thin">
+      Welcome Page
+    </h1>
+  </div>
+  );
+}
+
+export default Home;
