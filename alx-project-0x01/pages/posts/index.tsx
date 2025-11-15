7ea35c4473eafced84281124a77b23f049be6145
dijez <khadijaahmad2k21@gmail.com>
2025-11-15 12:56:36 +0100

    update

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,1 +1,15 @@
 // created index.tsx
+
+import Header from "@/components/layout/Header"
+
+const Posts: React.FC = () => {
+    return(
+        <div className="flex flex-col h-screen">
+            <Header />
+            <h1>posts page</h1>
+          
+        </div>
+    )
+}
+
+export default Posts
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,14 @@
 // created index.tsx
+
+import Header from "@/components/layout/Header"
+
+const Users: React.FC = () => {
+    return(
+        <div className="flex flex-col h-screen">
+            <Header />
+            <h1>users page</h1>
+        </div>
+    )
+}
+
+export default Users
